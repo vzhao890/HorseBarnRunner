7953b4c923d14c2d61afe4c5f786666230b9fa20
BT_1N3_22 <vzhao2047@bths.edu>
2018-10-24 12:24:30 -0400

    2

--- src/HorseRunner.java
+++ src/HorseRunner.java
@@ -23,4 +23,26 @@ public class HorseRunner
 
 
     }
+    public void consolidate() {
+        for (int i = 0; i < this.spaces.length-1; i++) {
+            if (this.spaces[i] == null) {
+                for (int j = i+1; j < this.spaces.length; j++) {
+                    if (this.spaces[j] != null) {
+                        this.spaces[i] = this.spaces[j];
+                        this.spaces[j] = null;
+                        j = this.spaces.length;
+                    }
+                }
+            }
+        }
+    }
+    public int findHorseSpace(String name) {
+        for (int i = 0; i < this.spaces.length; i++) {
+            if (this.spaces[i]!=null && name.equals(this.spaces[i].getName())) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
 }
