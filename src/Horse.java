d23334f7e380c5f0e43c387703b537134b03edae
BT_1N3_22 <vzhao2047@bths.edu>
2018-10-25 09:20:20 -0400

    2

---
+++ src/Horse.java
@@ -0,0 +1,18 @@
+public class Horse
+{
+    public interface Horse
+    {
+        public static String getName(names)
+        {
+            return name;
+        }
+        public static int getWeight(weight)
+        {
+            return weight;
+        }
+
+
+
+    }
+
+}
